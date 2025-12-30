22302a3f7acc89c470dd60000bbe4381ec59e283
Lohith Lokesh <lohith1024@Lohiths-MacBook-Air.local>
2025-12-30 14:02:09 +0530

    Fix persistent storage for production

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -1,28 +1,22 @@
 const fs = require("fs");
 const path = require("path");
 
-const dataDir = path.join(__dirname, "../data");
-const filePath = path.join(dataDir, "tasks.json");
+// Render allows writes ONLY in /tmp
+const filePath = path.join("/tmp", "tasks.json");
 
-// Ensure data directory & file exist
+// Ensure file exists
 const ensureFileExists = () => {
-    if (!fs.existsSync(dataDir)) {
-        fs.mkdirSync(dataDir);
-    }
-
     if (!fs.existsSync(filePath)) {
         fs.writeFileSync(filePath, JSON.stringify([]));
     }
 };
 
-// Read tasks safely
 const readTasks = () => {
     ensureFileExists();
     const data = fs.readFileSync(filePath, "utf-8");
     return JSON.parse(data);
 };
 
-// Write tasks safely
 const writeTasks = (tasks) => {
     ensureFileExists();
     fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
